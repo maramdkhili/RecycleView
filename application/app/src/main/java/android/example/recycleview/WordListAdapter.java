5ea25df34b5182e883aba8bef267413c889ecb3b
POST3 <post3.prog2@enetcom.usf.tn>
2022-11-17 16:48:21 +0100

    add_app

---
+++ application/app/src/main/java/android/example/recycleview/WordListAdapter.java
@@ -0,0 +1,25 @@
+package android.example.recycleview;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+    @NonNull
+    @Override
+    class WordViewHolder extends RecyclerView.ViewHolder {
+    public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
